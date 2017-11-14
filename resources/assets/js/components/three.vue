a519e2283c2eefd5e2a310699790e9adaf20f007
abdoumjr <abdoumjr@gmail.com>
2017-11-14 13:08:30 +0100

    final

--- resources/assets/js/components/three.vue
+++ resources/assets/js/components/three.vue
@@ -3,7 +3,7 @@
   <div class="container">
     <div class="content has-text-centered">
       <p>
-      VUeJs Phonebook app by <strong>abdoumjr</strong>
+      VUeJs Phonebook app by <a href="https://github.com/abdoumjr/Phonebook/tree/master">abdoumjr</a>
       </p>
     </div>
   </div>
