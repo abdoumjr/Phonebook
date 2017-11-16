41204856f390820e2771fe86ec4964ece85b6068
Moudjar Abdelkader <abdoumjr@gmail.com>
2017-11-17 00:03:08 +0100

    Update nav.blade.php

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar" role="navigation" aria-label="main navigation">
   <div class="navbar-brand">
     <a class="navbar-item" href="https://www.facebook.com/profile.php?id=100014399047129">
-      <img src="https://scontent-mrs1-1.xx.fbcdn.net/v/t1.0-9/22815566_283615628795070_9212366100309995691_n.jpg?oh=973b16c3f7f2b0d9de5f00f365539fe5&oe=5A6C1C2A" alt="Bulma: a modern CSS framework based on Flexbox" width="50" height="80">
+      <img src="https://int-dir.s3.amazonaws.com/uploads/930_930_Contacts-icon.png" alt="Bulma: a modern CSS framework based on Flexbox" width="50" height="80">
     </a>
     @auth
     <a class="navbar-item" >
